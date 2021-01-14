9d4d54999f49f7ac5f63d85319d9a7c463ae5c3b
Sthopeless <rjs.ricardosilva@gmail.com>
2021-01-14 03:31:11 +0100

    Initial commit

--- create_container.sh
+++ create_container.sh
@@ -65,7 +65,7 @@ TEMP_DIR=$(mktemp -d)
 pushd $TEMP_DIR >/dev/null
 
 # Download setup script
-wget -qL https://github.com/Sthopeless/proxmox_docker_ha/raw/main/setup.sh
+wget -qL https://github.com/Sthopeless/proxmox_docker_ha/raw/master/setup.sh
 
 # Detect modules and automatically load at boot
 load_module aufs
